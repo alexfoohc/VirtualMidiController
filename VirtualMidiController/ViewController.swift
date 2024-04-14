881d454410806ade5fe39e5fa68ce77b28aebee5
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2024-04-13 20:52:36 -0600

    Initial commit

---
+++ VirtualMidiController/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VirtualMidiController
+//
+//  Created by Alejandro Hernandez on 13/04/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
